3e6b505903e87274ae2850365f6e9310762d808b
Erich Healy <ErichRHealy@gmail.com>
2013-11-08 11:38:53 -0800

    Added persistance and folder creation functions

---
+++ cmis/__init__.py
@@ -0,0 +1,8 @@
+from ._version import __version__, __version_info__  # noqa
+from .util import get_or_create_folder, persist, persist_from_disk
+
+__all__ = [
+    'get_or_create_folder',
+    'persist',
+    'persist_from_disk'
+]
