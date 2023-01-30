78bb81620bd5c90824f4867032ca2b70c475ac3b
removedporn <86824510+removedporn@users.noreply.github.com>
2023-01-30 10:24:13 +0800

    Delete cmis directory

--- cmis/__init__.py
+++
@@ -1,8 +0,0 @@
-from ._version import __version__, __version_info__  # noqa
-from .utils import get_or_create_folder, persist, persist_from_disk
-
-__all__ = [
-    'get_or_create_folder',
-    'persist',
-    'persist_from_disk'
-]
